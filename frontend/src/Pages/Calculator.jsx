3f3f98c5d330e4f3e1666dff45152ba3e1964300
Viral Vaghela <viralvaghela8577@gmail.com>
2024-06-30 22:30:27 +0530

    v2.3.6 add - dynamics

--- frontend/src/Pages/Calculator.jsx
+++ frontend/src/Pages/Calculator.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Navbar } from "../Components/compIndex";
 import { handleEnter } from "../app.js";
 import "../App.css";
@@ -6,14 +6,11 @@ import CalculateFinal from "../Components/CalculateFinal.jsx";
 import { useAppContext } from "../context/AppContext.jsx";
 
 const Calculator = () => {
-  const totalQues = [1, 2, 3, 4, 5, 6, 7];
+  const [totalQues, setTotalQues] = useState([1, 2, 3, 4, 5]);
   useEffect(() => {
     handleEnter();
   });
   const { setAppQuestions } = useAppContext();
-  useEffect(() => {
-    setAppQuestions(totalQues);
-  }, []);
 
   const renderInputs = () => {
     return totalQues.map((num, index) => (
@@ -40,11 +37,31 @@ const Calculator = () => {
       </div>
     ));
   };
-
+  const handleQuestionsNums = (e) => {
+    const num = parseInt(e.target.value, 10);
+    if (!isNaN(num) && num > 0) {
+      const newTotalQues = Array.from({ length: num }, (_, i) => i + 1);
+      setTotalQues(newTotalQues);
+      setAppQuestions(newTotalQues);
+      console.log(`totalQues Array : ${newTotalQues}`);
+    }
+  };
   return (
     <>
       <Navbar />
-      <div className="flex inputKeys items-center mt-20">
+      <div className="totalOuterQuestions h-20 mt-20">
+        <div className="flex justify-center mt-10">
+          <input
+            id="que-num-input"
+            className="input border border-gray-600 p-2  rounded-lg w-30 max-w-xs "
+            placeholder="Enter No. Total Questions"
+            onChange={handleQuestionsNums}
+            type="number"
+            min="1"
+          />
+        </div>
+      </div>
+      <div className="flex inputKeys items-center mt-10">
         <div className="flex">
           <div className="w-1/2 p-2">
             <h3 className="mb-4 font-semibold text-center">Your Answer Key</h3>
