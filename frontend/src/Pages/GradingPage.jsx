51d936608cd00cb25b128163e73b2d887dfa16b1
Viral Vaghela <viralvaghela8577@gmail.com>
2024-07-01 18:43:29 +0530

    v2.6.1 update - frontend

--- frontend/src/Pages/GradingPage.jsx
+++ frontend/src/Pages/GradingPage.jsx
@@ -2,12 +2,10 @@ import "./OMRSheet.css";
 import { Navbar } from "../Components/compIndex";
 import { useAppContext } from "../context/AppContext";
 import { useEffect, useState } from "react";
-
 const GradingPage = () => {
   const { appMarksData, resultDisplay } = useAppContext();
   const [userAnswers, setUserAnswers] = useState([]);
   const [correctAnswers, setCorrectAnswers] = useState([]);
-
   useEffect(() => {
     console.log(resultDisplay);
     if (appMarksData) {
@@ -27,33 +25,25 @@ const GradingPage = () => {
       spans.push(<span key={i}>{`${i}`}</span>);
     }
     return (
-      <div
-        className="omr-row flex flex-col md:flex-row items-center md:justify-between"
-        key={index}
-      >
+      <div className="omr-row" key={index}>
         <div className="question-number">{spans[index]}</div>
-        <div className="flex">
-          {options.map((option) => {
-            let bubbleClass = "bubble";
-            if (userAnswers[index] === option) {
-              if (correctAnswers[index] === option) {
-                bubbleClass += " correct"; // Correct answer marked by user
-              } else {
-                bubbleClass += " incorrect"; // Incorrect answer marked by user
-              }
-            } else if (
-              !userAnswers[index] &&
-              correctAnswers[index] === option
-            ) {
-              bubbleClass += " unattempted"; // Correct answer not attempted
+        {options.map((option) => {
+          let bubbleClass = "bubble";
+          if (userAnswers[index] === option) {
+            if (correctAnswers[index] === option) {
+              bubbleClass += " correct"; // Correct answer marked by user
+            } else {
+              bubbleClass += " incorrect"; // Incorrect answer marked by user
             }
-            return (
-              <div className={`${bubbleClass} mx-4`} key={option}>
-                {option}
-              </div>
-            );
-          })}
-        </div>
+          } else if (!userAnswers[index] && correctAnswers[index] === option) {
+            bubbleClass += " unattempted"; // Correct answer not attempted
+          }
+          return (
+            <div className={`${bubbleClass} mx-4`} key={option}>
+              {option}
+            </div>
+          );
+        })}
       </div>
     );
   };
@@ -65,28 +55,24 @@ const GradingPage = () => {
       spans.push(<span key={i}>{`${i}`}</span>);
     }
     return (
-      <div
-        className="omr-row flex flex-col md:flex-row items-center md:justify-between"
-        key={index}
-      >
+      <div className="omr-row" key={index}>
         <div className="question-number">{spans[index]}</div>
-        <div className="flex">
-          {options.map((option) => {
-            let keyBubbleClass = "bubble";
-            if (correctAnswers[index] === option) {
+        {options.map((options) => {
+          let keyBubbleClass = "bubble";
+          if (correctAnswers[index] === options) {
+            if (correctAnswers[index] === correctAnswers[index]) {
               keyBubbleClass += " correct"; // Correct answer marked by user
             }
-            return (
-              <div className={`${keyBubbleClass} mx-4`} key={option}>
-                {option}
-              </div>
-            );
-          })}
-        </div>
+          }
+          return (
+            <div className={`${keyBubbleClass} mx-4`} key={options}>
+              {options}
+            </div>
+          );
+        })}
       </div>
     );
   };
-
   return (
     <>
       <Navbar />
@@ -100,47 +86,49 @@ const GradingPage = () => {
           <h1 className="lg:float-left">Results Summary</h1>
         </div>
         <div className="flex justify-center">
-          <div className="grid grid-cols-1 lg:grid-cols-4 text-black gap-4">
-            <div className="testResults mt-4 lg:mt-10 h-auto lg:h-5/6 lg:min-w-48 border p-3 rounded-lg bg-white text-center lg:px-20 shadow-xl">
-              <p className="text-lg lg:text-xl">Total Marks</p>
-              <h6 className="text-xl lg:text-2xl font-semibold mt-2">
+          <div className="grid grid-cols-4 text-black">
+            <div className="testResults mt-5 lg:mt-10 lg:h-5/6 lg:min-w-48  lg:mx-10 lg:border p-3 rounded-lg lg:bg-white text-center lg:px-20 lg:shadow-xl">
+              <p className="lg:text-xl text-sm font-semibold whitespace-nowrap">
+                Total Marks
+              </p>
+              <h6 className="lg:text-2xl lg:font-semibold mt-2 font-semibold">
                 {resultDisplay.totalMarks || "0"}
               </h6>
             </div>
-            <div className="testResults mt-4 lg:mt-10 h-auto lg:h-5/6 lg:min-w-48 border p-3 rounded-lg bg-white text-center lg:px-20 shadow-xl">
-              <p className="text-lg lg:text-xl">Correct Answers</p>
-              <h6 className="text-xl lg:text-2xl font-semibold text-green-600 mt-2">
+            <div className="testResults mt-5 whitespace-nowrap lg:mt-10 lg:h-5/6 lg:min-w-48 lg:text-2xl lg:mx-10 lg:border p-3 rounded-lg lg:bg-white text-center lg:px-20 lg:shadow-xl">
+              <p className="lg:text-xl text-sm whitespace-nowrap caption-toolkit font-semibold">
+                Correct <span className="hidden md:inline">Answers</span>
+              </p>
+              <h6 className="lg:text-2xl lg:font-semibold text-green-600 mt-2 font-semibold">
                 {resultDisplay.totalCorrect || "0"}
               </h6>
             </div>
-            <div className="testResults mt-4 lg:mt-10 h-auto lg:h-5/6 lg:min-w-48 border p-3 rounded-lg bg-white text-center lg:px-20 shadow-xl">
-              <p className="text-lg lg:text-xl">Incorrect Answers</p>
-              <h6 className="text-xl lg:text-2xl font-semibold text-yellow-500 mt-2">
+            <div className="testResults mt-5 lg:mt-10 lg:h-5/6 lg:min-w-48 lg:text-2xl lg:mx-10 lg:border p-3 rounded-lg lg:bg-white text-center lg:px-20 lg:shadow-xl">
+              <p className="lg:text-xl text-sm whitespace-nowrap caption-toolkit font-semibold">
+                Inorrect <span className="hidden md:inline">Answers</span>
+              </p>
+              <h6 className="lg:text-2xl lg:font-semibold font-semibold text-yellow-500 mt-2">
                 {resultDisplay.totalIncorrect || "0"}
               </h6>
             </div>
-            <div className="testResults mt-4 lg:mt-10 h-auto lg:h-5/6 lg:min-w-48 border p-3 rounded-lg bg-white text-center lg:px-20 shadow-xl">
-              <p className="text-lg lg:text-xl">Unattempted</p>
-              <h6 className="text-xl lg:text-2xl font-semibold text-gray-500 mt-2">
+            <div className="testResults mt-5 lg:mt-10 lg:h-5/6 lg:min-w-48 lg:text-2xl lg:mx-10 lg:border p-3 rounded-lg lg:bg-white text-center lg:px-20 lg:shadow-xl">
+              <p className="lg:text-xl text-sm font-semibold">Unattempted</p>
+              <h6 className="lg:text-2xl lg:font-semibold text-gray-500 mt-2 font-semibold">
                 {resultDisplay.totalUnattempted || "0"}
               </h6>
             </div>
           </div>
         </div>
       </div>
-      <div className="flex flex-col lg:flex-row displayOMR justify-center mt-20 gap-10">
+      <div className="flex displayOMR justify-center mt-10">
         <div className="grading-page">
-          <h1 className="title text-xl font-semibold text-center lg:text-left">
-            Your Answer Key
-          </h1>
+          <h1 className="title text-xl font-semibold">Your Answer Key</h1>
           <div className="omr-sheet">
             {correctAnswers.map((_, index) => renderOMRRow(index))}
           </div>
         </div>
         <div className="grading-page">
-          <h1 className="title text-xl font-semibold text-center lg:text-left">
-            Real Answer Key
-          </h1>
+          <h1 className="title text-xl font-semibold">Real Answer Key</h1>
           <div className="omr-sheet">
             {correctAnswers.map((_, index) => renderCorrectKey(index))}
           </div>
