fcdbe57774e5d7bd1a708e8ad750b49d6e06f63e
Viral Vaghela <viralvaghela8577@gmail.com>
2024-07-19 12:57:48 +0530

    v5.2.3 fix - server proxy

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import cors from 'cors'
 dotenv.config();
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://avvise.vercel.app',
     method: 'GET,POST,DELETE,PATCH,HEAD',
     Credentials: true,
 }
