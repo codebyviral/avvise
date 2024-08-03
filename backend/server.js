eac26e3c02de87322f6054cc37078f1e4030ea03
Viral Vaghela <viralvaghela8577@gmail.com>
2024-08-03 22:21:58 +0530

    backend - fix - server - proxy

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import cors from 'cors'
 dotenv.config();
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://localhost:5173',
     method: 'GET, POST, DELETE, PATCH, HEAD',
     Credentials: true,
     allowedHeaders: 'Content-Type, Authorization'
