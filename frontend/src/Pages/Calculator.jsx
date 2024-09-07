ad827575231da943fbbc00bc63bae68eea6df71c
Viral Vaghela <viralvaghela8577@gmail.com>
2024-09-07 18:49:39 +0530

    "Refactor Calculator.jsx: import Steps from Driver-Step.js and replace hardcoded steps with Steps variable"

--- frontend/src/Pages/Calculator.jsx
+++ frontend/src/Pages/Calculator.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Navbar, Footer, Modal } from "../Components/compIndex";
 import { handleEnter } from "../app.js";
 import "../App.css";
+import { Steps } from "../assets/Driver-Step.js";
 import CalculateFinal from "../Components/CalculateFinal.jsx";
 import { useAppContext } from "../context/AppContext.jsx";
 import { driver } from "driver.js";
@@ -34,56 +35,7 @@ const Calculator = () => {
   };
   const driverObj = driver({
     showProgress: true,
-    steps: [
-      {
-        element: ".input-ques",
-        popover: {
-          title: "Getting Started by Entering the total number of Questions.",
-          description:
-            "Enter the number of total questions you have for example. 5",
-        },
-      },
-      {
-        element: ".marking_sys",
-        popover: {
-          title: "Choose your Marking system depending on your Exam.",
-          description:
-            "Select the marking system that corresponds to your exam requirements. For example, you might use a system like No Negative for Boards, JEE-NEET or even UPSC.",
-        },
-      },
-      {
-        element: ".newSystem-btn",
-        popover: {
-          title: "Incase you don't find your marking system or EXAM NAME",
-          description:
-            "Please click on this link and fill out the form to update our web app with your marking system. This will not only help you but also assist us in improving our app to reach more students.",
-        },
-      },
-      {
-        element: ".your-answer-key",
-        popover: {
-          title: "Enter Your Own Answer Key in Sequence",
-          description:
-            "Enter your answer key in sequence. For example, if your answer key is 'ADBC,' enter 'A' in input 1, 'D' in input 2, and so on.",
-        },
-      },
-      {
-        element: ".real-answer-key",
-        popover: {
-          title: "Enter Your Actual Answer Key",
-          description:
-            "Enter your answer key in sequence. For example, if your answer key is 'ABBC,' enter 'A' in input 1, 'B' in input 2, and so on.",
-        },
-      },
-      {
-        element: ".calculate-btn",
-        popover: {
-          title: "Calculate Your Result",
-          description:
-            "Once you have entered all your answers, click the 'Calculate' button to generate your result.",
-        },
-      },
-    ],
+    steps: Steps,
   });
   const driveTour = () => {
     driverObj.drive();
---
+++ frontend/src/assets/Driver-Step.js
@@ -0,0 +1,51 @@
+const Steps = [
+    {
+        element: ".input-ques",
+        popover: {
+            title: "Getting Started by Entering the total number of Questions.",
+            description:
+                "Enter the number of total questions you have for example. 5",
+        },
+    },
+    {
+        element: ".marking_sys",
+        popover: {
+            title: "Choose your Marking system depending on your Exam.",
+            description:
+                "Select the marking system that corresponds to your exam requirements. For example, you might use a system like No Negative for Boards, JEE-NEET or even UPSC.",
+        },
+    },
+    {
+        element: ".newSystem-btn",
+        popover: {
+            title: "Incase you don't find your marking system or EXAM NAME",
+            description:
+                "Please click on this link and fill out the form to update our web app with your marking system. This will not only help you but also assist us in improving our app to reach more students.",
+        },
+    },
+    {
+        element: ".your-answer-key",
+        popover: {
+            title: "Enter Your Own Answer Key in Sequence",
+            description:
+                "Enter your answer key in sequence. For example, if your answer key is 'ADBC,' enter 'A' in input 1, 'D' in input 2, and so on.",
+        },
+    },
+    {
+        element: ".real-answer-key",
+        popover: {
+            title: "Enter Your Actual Answer Key",
+            description:
+                "Enter your answer key in sequence. For example, if your answer key is 'ABBC,' enter 'A' in input 1, 'B' in input 2, and so on.",
+        },
+    },
+    {
+        element: ".calculate-btn",
+        popover: {
+            title: "Calculate Your Result",
+            description:
+                "Once you have entered all your answers, click the 'Calculate' button to generate your result.",
+        },
+    },
+]
+export { Steps }
