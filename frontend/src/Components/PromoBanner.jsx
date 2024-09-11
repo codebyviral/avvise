c8c4303640bd88903dbc28324a061b590cf0bbf4
Viral Vaghela <viralvaghela8577@gmail.com>
2024-09-11 11:32:24 +0530

    fix - frontend (typo)

--- frontend/src/Components/PromoBanner.jsx
+++ frontend/src/Components/PromoBanner.jsx
@@ -53,7 +53,7 @@ const PromoBanner = () => {
               href="/aboutus"
               className="flex-none rounded-full bg-gray-900 px-3.5 py-1 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-900"
             >
-              Explored Now <span aria-hidden="true">&rarr;</span>
+              Explore Now <span aria-hidden="true">&rarr;</span>
             </a>
           </>
         ) : (
