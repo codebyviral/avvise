ead2995832bb169cacf5fbf1fb31dec1d6c46344
codebyviral <viralvaghela2641@gmail.com>
2026-03-05 22:32:02 +0530

    update - env file

--- frontend/src/Pages/Admin.jsx
+++ frontend/src/Pages/Admin.jsx
@@ -5,7 +5,7 @@ const Admin = () => {
   const [users, setUsers] = useState([]);
   const [isAdmin, setIsAdmin] = useState(false);
   const fetchUsers = async () => {
-    const usersUrlAdmin = "http://localhost:4000/api/users/admin";
+    const usersUrlAdmin = "https://avvise-backend.vercel.app/api/users/admin";
     try {
       const response = await fetch(usersUrlAdmin, {
         method: "GET",
